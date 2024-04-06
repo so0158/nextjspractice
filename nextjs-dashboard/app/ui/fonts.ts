bdf3e639354635b3cb2268dba8bd1aeec48706d0
ssoyoung <sunshine.lee.sl@gmail.com>
2024-04-06 11:00:34 +0900

    44

--- nextjs-dashboard/app/ui/fonts.ts
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -1,5 +1,6 @@
-//import { Inter } from 'next/font/google';
-import { Inter, Lusitana } from 'next/font/google';
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+//import { Inter, Lusitana } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
 
